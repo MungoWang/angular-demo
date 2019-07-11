a26e6cfbd404a9632d1657a1cc827acf6b7af4ce
MungoWang <panexwang@163.com>
2019-07-11 16:16:25 +0800

    Updated UT for testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'angular-demo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-demo');
+    expect(app.title).toEqual('angular-demo-wrong');
   });
 
   it('should render title in a h1 tag', () => {
